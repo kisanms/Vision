9e7fc78f26ad97a652cd2ccaf5b7e94bc6e9da94
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-04-21 21:55:45 +0530

    microphone permissiom

--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Image,
   StatusBar,
   StyleSheet,
@@ -10,8 +11,50 @@ import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { scale, verticalScale } from "react-native-size-matters";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Audio } from "expo-av";
 
 export default function HomeScreen() {
+  const [text, setText] = React.useState("");
+  const [isRecording, setIsRecording] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [recording, setRecording] = React.useState("");
+  const [AIResponse, setAIResponse] = React.useState(false);
+
+  //Microphone permission
+  const getMicerophonePermission = async () => {
+    try {
+      const { granted } = await Audio.requestPermissionsAsync();
+      if (!granted) {
+        Alert.alert(
+          "Permission",
+          "Please allow microphone access to use this feature."
+        );
+        return false;
+      }
+      return true;
+    } catch (error) {
+      console.log("Error getting microphone permission:", error);
+      Alert.alert(
+        "Permission",
+        "An error occurred while requesting microphone access."
+      );
+      return false;
+    }
+  };
+
+  const startRecording = async () => {
+    const hasPermission = await getMicerophonePermission();
+    if (!hasPermission) return;
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      setIsRecording(true);
+
+      const { recording } = await Audio.Recording.createAsync();
+    } catch (error) {}
+  };
   return (
     <LinearGradient
       colors={["#250152", "#000000"]}
