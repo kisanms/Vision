0719c19815996a6fd70bef12170a21a7b7e5b5ac
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-04-21 21:43:10 +0530

    mic icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="(routes)/onboarding/index" />
-        <Stack.Screen name="(routes)/home/inex" />
+        <Stack.Screen name="(routes)/home/index" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -1,7 +1,15 @@
-import { Image, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { scale, verticalScale } from "react-native-size-matters";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function HomeScreen() {
   return (
@@ -13,7 +21,7 @@ export default function HomeScreen() {
     >
       <StatusBar barStyle="light-content" />
 
-      {/**back shoadows */}
+      {/**back shoadows  pictures*/}
       <Image
         source={require("@/assets/main/Ellipse 10.png")}
         style={{
@@ -32,6 +40,41 @@ export default function HomeScreen() {
           width: scale(210),
         }}
       />
+      <View style={{ marginTop: verticalScale(-40) }}>
+        <TouchableOpacity
+          style={{
+            width: scale(110),
+            height: scale(110),
+            flexDirection: "row",
+            backgroundColor: "#fff",
+            borderRadius: scale(100),
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <FontAwesome name="microphone" size={scale(50)} color="#2b3356" />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          alignItems: "center",
+          width: scale(350),
+          position: "absolute",
+          bottom: verticalScale(90),
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: scale(16),
+            width: scale(269),
+            textAlign: "center",
+            lineHeight: 25,
+          }}
+        >
+          Press the microphone to start recording !
+        </Text>
+      </View>
     </LinearGradient>
   );
 }
